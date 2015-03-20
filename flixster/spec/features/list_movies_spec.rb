fc8562434afab2c7193dbb3f62fd41ecf58cb8b3
Chris Salvi <csalvi42@gmail.com>
2015-03-20 15:49:07 -0700

    refactored code

--- flixster/spec/features/list_movies_spec.rb
+++ flixster/spec/features/list_movies_spec.rb
@@ -6,6 +6,9 @@ describe "Viewing the list of movies" do
     visit 'http://example.com/movies'
 
     expect(page).to have_text("3 Movies")
+    expect(page).to have_text("Movie 1")
+    expect(page).to have_text("Movie 2")
+    expect(page).to have_text("Movie 3")
   end
   
 end
