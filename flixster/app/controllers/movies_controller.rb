bfd870a0218feed27081427d447d5bde9a79726f
Chris Salvi <csalvi42@gmail.com>
2015-03-23 21:35:24 -0700

    created form for new movie

--- flixster/app/controllers/movies_controller.rb
+++ flixster/app/controllers/movies_controller.rb
@@ -13,8 +13,18 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
     redirect_to @movie
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+end
+
+private
+
+def movie_params
+  movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
 end
